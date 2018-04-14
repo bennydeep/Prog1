a57dd86649e4129056a61d75b85d56e2c04ffdb9
benny <ben.komjathy@gmail.com>
2018-04-14 11:36:39 +0200

    asdasdasd

--- Beadandó-EX3.py
+++ Beadandó-EX3.py
@@ -1,16 +1,14 @@
-def inframe(text):
-    str=''
-    szavak=[]
-    for i in text:
-        if i==' ':
-            szavak.append(str)
-            str=''
-        else:
-            str+=i
-    szavak.append(str)
-
-
-
-
 text=input("Írj be valamit")
-print(inframe(text))
+
+szo=''
+szavak=[]
+for i in text:
+    if i==' ':
+        szavak.append(szo)
+        szo=''
+    else:
+        szo+=i
+szavak.append(szo)
+
+
+print(szavak)
